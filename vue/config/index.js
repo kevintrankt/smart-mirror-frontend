13ff890d261747380bcc6229796e036d2ee10ab1
Kevin Tran <kevin.tran.kt@gmail.com>
2018-05-23 00:30:53 -0700

    create production build using npm run build

--- vue/config/index.js
+++ vue/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/smart-mirror-frontend/vue/dist/',
     proxyTable: {},
 
     // Various Dev Server settings
